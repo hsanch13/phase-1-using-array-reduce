dc49bb8bac15da24e76fa12f639b3513164400d9
hsanch13 <hayleysanchez13@gmail.com>
2023-11-17 11:06:50 -0700

    wrote a function using the reduce method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+    const totalBatteries = batteryBatches.reduce((accumulator, battery) =>
+        accumulator + battery, 0)
 // Code your solution here
